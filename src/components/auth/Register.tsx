5aa56e799c2ead7ff9ddea7aa33ad28db30b5be5
Jaiver Ramos <jaiver.ramos7942@gmail.com>
2024-08-06 18:07:36 -0500

    solucionado el build

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,13 +1,17 @@
-import { z } from "zod"
-import { registerFormSchema as formSchema  } from "@/lib/zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { AuthError, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
-import { useAuth, useFirestore, useStorage } from "reactfire"
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
-import { useLoadingStore } from "@/store/loading-store"
+import { z } from "zod";
+import { registerFormSchema as formSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  AuthError,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { useAuth, useFirestore, useStorage } from "reactfire";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useLoadingStore } from "@/store/loading-store";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -15,192 +19,198 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { UserDB } from "@/schemas/fireStore-schema"
-import { doc, setDoc } from "firebase/firestore"
-import { CgSpinner } from "react-icons/cg"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { doc, setDoc } from "firebase/firestore";
+import { CgSpinner } from "react-icons/cg";
+import type { UserDB } from "../../schemas/firestore-schema";
 
 interface Props {
-    handleClick: () => void
+  handleClick: () => void;
 }
 
-
 const Register = ({ handleClick }: Props) => {
+  const auth = useAuth();
+  const db = useFirestore();
+  const storage = useStorage();
+  const { setLoading, loading } = useLoadingStore();
 
-    const auth = useAuth()
-    const db = useFirestore()
-    const storage = useStorage()
-    const { setLoading, loading } = useLoadingStore()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      displayName: "",
+      photoURL: undefined,
+    },
+  });
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          email: "",
-          password: "",
-          confirmPassword: "",
-          displayName: "", 
-          photoURL: undefined
-        },
-      })
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      setLoading(true);
 
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-        try {
-            setLoading(true) 
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
 
-            const { user } = await createUserWithEmailAndPassword(auth, values.email, values.password)
-            
-            // guardar en etorage
-            const storageRef = ref(storage, `users/${user.uid}.jpg`)
-            await uploadBytes(storageRef, values.photoURL)
+      // guardar en etorage
+      const storageRef = ref(storage, `users/${user.uid}.jpg`);
+      await uploadBytes(storageRef, values.photoURL);
 
-            // Recuperar foto del de firebase
-            const photoURL = await getDownloadURL(storageRef)
+      // Recuperar foto del de firebase
+      const photoURL = await getDownloadURL(storageRef);
 
-            await updateProfile(user, {
-                displayName: values.displayName,
-                photoURL: photoURL
-            })
+      await updateProfile(user, {
+        displayName: values.displayName,
+        photoURL: photoURL,
+      });
 
-            // Guardar la colección de usuarios
-            const userDB: UserDB = {
-                displayName: values.displayName,
-                email: values.email,
-                photoURL: photoURL,
-                uid: user.uid,
-                firends: [],
-                rooms: [] 
-            }
+      // Guardar la colección de usuarios
+      const userDB: UserDB = {
+        displayName: values.displayName,
+        email: values.email,
+        photoURL: photoURL,
+        uid: user.uid,
+        firends: [],
+        rooms: [],
+      };
 
-            const userDBRef = doc(db, "users", user.uid)
-            await setDoc(userDBRef, userDB)
+      const userDBRef = doc(db, "users", user.uid);
+      await setDoc(userDBRef, userDB);
 
-            setLoading(false)
-        } catch (error) {
+      setLoading(false);
+    } catch (error) {
+      const firebaseError = error as AuthError;
 
-            const firebaseError = error as AuthError
-
-            if(firebaseError.code === "auth/email-already-in-use") {
-                form.setError("email",{
-                    type: "manual",
-                    message: "El correo electrónico ya está registrado"
-                })
-                return
-            }            
-        } finally {
-            setLoading(false)       
-        }
+      if (firebaseError.code === "auth/email-already-in-use") {
+        form.setError("email", {
+          type: "manual",
+          message: "El correo electrónico ya está registrado",
+        });
+        return;
+      }
+    } finally {
+      setLoading(false);
     }
+  }
 
   return (
     <div>
-        <Card className="shadow-black shadow-sm">
-            <CardHeader>
-                <CardTitle>Registrarse</CardTitle>
-                <CardDescription>Rellene todos los campos del formulario.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="displayName"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Nombre de usuario</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Jose Jose" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+      <Card className="shadow-black shadow-sm">
+        <CardHeader>
+          <CardTitle>Registrarse</CardTitle>
+          <CardDescription>
+            Rellene todos los campos del formulario.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="displayName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre de usuario</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Jose Jose" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Correo Electrónico</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="user@domain.com" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Correo Electrónico</FormLabel>
+                    <FormControl>
+                      <Input placeholder="user@domain.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Contraseña</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="*******" {...field} type="password" />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Contraseña</FormLabel>
+                    <FormControl>
+                      <Input placeholder="*******" {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                        <FormField
-                            control={form.control}
-                            name="confirmPassword"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Confirme la contraseña</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="*******" {...field} type="password" />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirme la contraseña</FormLabel>
+                    <FormControl>
+                      <Input placeholder="*******" {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                        <FormField
-                            control={form.control}
-                            name="photoURL"
-                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                            render={({ field: { value, onChange, ...fieldProps } }) => (
-                                <FormItem>
-                                <FormLabel>Foto</FormLabel>
-                                <FormControl>
-                                    <Input
-                                    {...fieldProps}
-                                    placeholder="Foto"
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={(event) =>
-                                        onChange(event.target.files && event.target.files[0])
-                                    }
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+              <FormField
+                control={form.control}
+                name="photoURL"
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                render={({ field: { value, onChange, ...fieldProps } }) => (
+                  <FormItem>
+                    <FormLabel>Foto</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...fieldProps}
+                        placeholder="Foto"
+                        type="file"
+                        accept="image/*"
+                        onChange={(event) =>
+                          onChange(event.target.files && event.target.files[0])
+                        }
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                        <div className="flex justify-between">
-                            <Button onClick={handleClick} variant="outline" type="button">Iniciar sesión</Button>
-                            <Button type="submit" disabled={loading}>
-                                { loading ? <CgSpinner size="sm" /> : <span>Registrar</span> }
-                            </Button>
-                        </div>
-                    </form>
-                </Form>
-            </CardContent>
-        </Card>
+              <div className="flex justify-between">
+                <Button onClick={handleClick} variant="outline" type="button">
+                  Iniciar sesión
+                </Button>
+                <Button type="submit" disabled={loading}>
+                  {loading ? <CgSpinner size="sm" /> : <span>Registrar</span>}
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/components/chat/FiendSearh.tsx
+++ src/components/chat/FiendSearh.tsx
@@ -1,132 +1,140 @@
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { searchFormSchema as formSchema  } from "@/lib/zod"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { searchFormSchema as formSchema } from "@/lib/zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { addDoc, arrayUnion, collection, doc, getDocs, query, updateDoc, where } from "firebase/firestore"
-import { useAuth, useFirestore } from "reactfire"
-import { RoomDB, UserRooms } from "@/schemas/fireStore-schema"
-
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useAuth, useFirestore } from "reactfire";
+import { RoomDB, UserRooms } from "../../schemas/firestore-schema";
 
 const FiendSearh = () => {
+  const db = useFirestore();
+  const auth = useAuth();
 
-    const db = useFirestore()
-    const auth = useAuth()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  });
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            search: "",
-        },
-    })
-    
-    
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-        try {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (auth.currentUser?.email === values.search) {
+        form.setError("search", {
+          type: "manual",
+          message: "No se puede enviar un mensaje a uno mismo",
+        });
+        return;
+      }
 
-            if(auth.currentUser?.email === values.search) {
-                form.setError("search", {
-                    type: "manual",
-                    message: "No se puede enviar un mensaje a uno mismo"
-                })
-                return
-            }
+      const q = query(
+        collection(db, "users"),
+        where("email", "==", values.search)
+      );
 
-            const q = query(collection(db, "users"), where("email", "==", values.search))
+      const querySnapshot = await getDocs(q);
 
-            const querySnapshot = await getDocs(q)
+      if (querySnapshot.empty) {
+        form.setError("search", {
+          type: "manual",
+          message: "No se encontró ningún usuario con ese correo electrónico",
+        });
+        return;
+      }
 
-            if(querySnapshot.empty) {
-                form.setError("search", {
-                    type: "manual",
-                    message: "No se encontró ningún usuario con ese correo electrónico"
-                })
-                return
-            }
+      const friendDB = querySnapshot.docs[0].data();
 
-            const friendDB = querySnapshot.docs[0].data()
+      // Verifivar si ya son amigos
+      if (friendDB.firends.includes(auth.currentUser?.uid)) {
+        form.setError("search", {
+          type: "manual",
+          message: "Ya somos amigos",
+        });
+        return;
+      }
 
-            // Verifivar si ya son amigos
-            if(friendDB.firends.includes(auth.currentUser?.uid)) {  
-                form.setError("search", {
-                    type: "manual",
-                    message: "Ya somos amigos"
-                })
-                return
-            }
+      // Crear la sala
+      const newRoomDB: RoomDB = {
+        messages: [],
+        users: [auth.currentUser?.uid, friendDB.uid],
+      };
 
-            // Crear la sala
-            const newRoomDB: RoomDB = {
-                messages: [],
-                users: [auth.currentUser?.uid, friendDB.uid],
-            }
+      const roomRef = await addDoc(collection(db, "rooms"), newRoomDB);
 
-            const roomRef = await addDoc(collection(db, "rooms"), newRoomDB)
+      // Agregar la sala al usuario
+      const currentUserRoom: UserRooms = {
+        roomId: roomRef.id,
+        lastMessage: "",
+        timestamp: "",
+        fiendId: friendDB.uid,
+      };
 
-            // Agregar la sala al usuario
-            const currentUserRoom: UserRooms = {
-                 roomId: roomRef.id,
-                 lastMessage: "",
-                 timestamp: "",
-                 fiendId: friendDB.uid
-            }
+      const fiendRoom: UserRooms = {
+        roomId: roomRef.id,
+        lastMessage: "",
+        timestamp: "",
+        fiendId: auth.currentUser!.uid,
+      };
 
-            const fiendRoom: UserRooms = {
-                roomId: roomRef.id,
-                lastMessage: "",
-                timestamp: "",
-                fiendId: auth.currentUser!.uid
-            }
+      const currentUserIdRef = doc(db, "users", auth.currentUser!.uid);
+      const fiendRef = doc(db, "users", friendDB.uid);
 
-            const currentUserIdRef = doc(db, "users", auth.currentUser!.uid)
-            const fiendRef = doc(db, "users", friendDB.uid)
+      await updateDoc(currentUserIdRef, {
+        rooms: arrayUnion(currentUserRoom),
+        firends: arrayUnion(friendDB.uid),
+      });
 
-            await updateDoc(currentUserIdRef, {
-                rooms: arrayUnion(currentUserRoom),
-                firends: arrayUnion(friendDB.uid)
-            })
+      await updateDoc(fiendRef, {
+        rooms: arrayUnion(fiendRoom),
+        firends: arrayUnion(auth.currentUser!.uid),
+      });
 
-            await updateDoc(fiendRef, {
-                rooms: arrayUnion(fiendRoom),
-                firends: arrayUnion(auth.currentUser!.uid)
-            })
-
-            form.reset()
-
-        } catch (error) {
-            console.log(error)
-        }
+      form.reset();
+    } catch (error) {
+      console.log(error);
     }
+  }
 
   return (
     <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-            <FormField
-            control={form.control}
-            name="search"
-            render={({ field }) => (
-                <FormItem>
-                <FormControl>
-                    <Input placeholder="Buscar por correo" {...field} />
-                </FormControl>
-                <FormMessage />
-                </FormItem>
-            )}
-            />
-            <Button type="submit" className="w-full">Buscar</Button>
-        </form>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Buscar por correo" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Buscar
+        </Button>
+      </form>
     </Form>
-  )
-}
+  );
+};
 
-export default FiendSearh
+export default FiendSearh;
